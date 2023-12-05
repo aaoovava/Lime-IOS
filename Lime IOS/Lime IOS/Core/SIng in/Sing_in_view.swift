71e25427cf61ba0d34ee8ab429edc3460bcc266f
wasiliylolita <WasiliyLolita6@gmail.com>
2023-12-05 15:45:16 +0100

    add a navigation and fix small screens

--- Lime IOS/Lime IOS/Core/SIng in/Sing_in_view.swift
+++ Lime IOS/Lime IOS/Core/SIng in/Sing_in_view.swift
@@ -14,55 +14,58 @@ struct ContentView: View {
     private var width: Int = Int(UIScreen.main.bounds.width)
     
     var body: some View {
-        VStack {
-            Image("Sing in imge")
-                .resizable()
-                .scaledToFit()
-                .frame(width: CGFloat(width) * 0.8, height: CGFloat(height) * 0.15)
-                .padding(.vertical, 180)
-                .padding(.bottom, -CGFloat(width) * 0.25)
-            NavigationLink{
-                
-            }label:{
-                HStack{
-                    Image("Txt to sing up")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: CGFloat(width) * 0.6,height: CGFloat(height) * 0.025)
+        NavigationView{
+            VStack {
+                Image("Sing in imge")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: CGFloat(width) * 0.8, height: CGFloat(height) * 0.15)
+                    .padding(.vertical, CGFloat(height) * 0.2)
+                    .padding(.bottom, -CGFloat(width) * 0.25)
+                NavigationLink(destination: Sing_up_view()){
+                    HStack{
+                        Image("Txt to sing up")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: CGFloat(width) * 0.6,height: CGFloat(height) * 0.025)
+                    }
+                    
                 }
                 
-            }
-            
-            VStack(spacing: 30){
-               InputView(text: $phone, title: "Телефон", plaseHolder: "")
-                    .padding(.top,30)
-                    .padding(.horizontal,CGFloat(width) * 0.15)
-                InputView(text: $pass, title: "Пароль", plaseHolder: "",isSecureField: true)
-                    .padding(.horizontal,CGFloat(width) * 0.15)
-            }
-            NavigationLink{
-                
-            }label:{
-                HStack{
-                    Image("NewPass")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: CGFloat(width) * 0.6,height: CGFloat(height) * 0.019)
-                        .padding(.top,10)
+                VStack(spacing: CGFloat(height) * 0.02){
+                    InputView(text: $phone, title: "Телефон", plaseHolder: "")
+                        .padding(.top,30)
+                        .padding(.horizontal,CGFloat(width) * 0.15)
+                    InputView(text: $pass, title: "Пароль", plaseHolder: "",isSecureField: true)
+                        .padding(.horizontal,CGFloat(width) * 0.15)
+                }
+                NavigationLink{
+                    
+                }label:{
+                    HStack{
+                        Image("NewPass")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: self.height <= 700 ? CGFloat(self.height) * 0.2 : CGFloat(self.height) * 0.15,height:self.height <= 700 ? CGFloat(self.height) * 0.02 : CGFloat(self.height) * 0.04)
+                            .padding(.top,10)
+                    }
+                    
                 }
                 
-            }
-            
-            
+                
                 Spacer()
-            SwipeButton(title: "Увійти")
-                .padding(.bottom, 50)
-            
-            
+                SwipeButton(title: "Увійти")
+                    .padding(.bottom, height <= 700 ? CGFloat(height) * 0.03 : CGFloat(height) * 0.05)
+                
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(#colorLiteral(red: 0.0862745098, green: 0.0941176471, blue: 0.0941176471, alpha: 1)))
+            .edgesIgnoringSafeArea(.all)
+            .edgesIgnoringSafeArea(.bottom)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color(#colorLiteral(red: 0.0862745098, green: 0.0941176471, blue: 0.0941176471, alpha: 1)))
-                .edgesIgnoringSafeArea(.all)
+        .navigationBarTitle("", displayMode: .inline) // Пустой текст в качестве заголовка
+            .navigationBarBackButtonHidden(true)
     }
 }
 
