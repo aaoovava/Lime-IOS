93460a171fc471ce92ea6436c0b7dd19497a0fdd
wasiliylolita <WasiliyLolita6@gmail.com>
2024-08-01 19:03:20 +0200

    upd AccountService with base functions

--- Lime IOS/AccountService.swift
+++ Lime IOS/AccountService.swift
@@ -1,8 +1,37 @@
-//
-//  AccountService.swift
-//  Lime IOS
-//
-//  Created by Wasiliy Lolita on 27.07.2024.
-//
-
 import Foundation
+import FirebaseCore
+import FirebaseAuth
+
+class AccountService {
+    static let shared = AccountService()
+    private init() {}
+    
+    func signIn(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email + "@gmail.com", password: password) { authResult, error in
+            if let error = error {
+                print(error)
+            } else if let authResult = authResult {
+                print("success")
+            }
+        }
+    }
+    
+    func signUp(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email + "@gmail.com", password: password) { authResult, error in
+            if let error = error {
+                print(error)
+            } else if let authResult = authResult {
+                print("success")
+            }
+        }
+    }
+    
+    func signOut(completion: @escaping (Result<Void, Error>) -> Void) {
+        do {
+            try Auth.auth().signOut()
+            print("success")
+        } catch {
+            print(error)
+        }
+    }
+}
